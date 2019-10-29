540801b5dfba23006695a60efe7a4c2fa686f1a7
adkr <adrian.kremblewski@codeplant.io>
2019-10-29 08:47:45 +0100

    Improved API docs

--- src/main/java/pl/adkr/hsbc/challenge/config/SwaggerConfig.java
+++ src/main/java/pl/adkr/hsbc/challenge/config/SwaggerConfig.java
@@ -4,7 +4,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
-import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
 import springfox.documentation.service.Contact;
 import springfox.documentation.spi.DocumentationType;
@@ -16,14 +15,25 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 public class SwaggerConfig {
 
     @Bean
-    public Docket api() {
+    public Docket postingApi() {
         return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("posting")
+                .apiInfo(apiEndPointsInfo())
                 .select()
-                .apis(RequestHandlerSelectors
-                        .basePackage("pl.adkr.hsbc.challenge.posting.rest, pl.adkr.hsbc.challenge.following.rest"))
-                .paths(PathSelectors.regex("/.*"))
-                .build().apiInfo(apiEndPointsInfo());
+                .paths(PathSelectors.ant("/api/v**/message/**"))
+                .build();
     }
+
+    @Bean
+    public Docket followingApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .groupName("following")
+                .apiInfo(apiEndPointsInfo())
+                .select()
+                .paths(PathSelectors.ant("/api/v**/user/**"))
+                .build();
+    }
+
     private ApiInfo apiEndPointsInfo() {
         return new ApiInfoBuilder().title("Code Challenge")
                 .description("Twitter-like REST API")
